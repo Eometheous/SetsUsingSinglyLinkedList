ca9d90a76f25a0fe05e526bfefd65aab5500101e
Eometheous <jonathan@sthomasphoto.com>
2022-09-12 13:11:21 -0700

    Continued working on SetTest

--- src/main/java/SetTest.java
+++ src/main/java/SetTest.java
@@ -1,9 +1,14 @@
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 public class SetTest {
-    Set<String> set = new Set<>();
+    Set<String> set;
+    @BeforeAll
+    public void setUp() {
+        set = new Set<>();
+    }
 
     @Test
     public void testAddItem() {
@@ -26,13 +31,15 @@ public class SetTest {
     @Test
     public void testRemove() {
         set.addItem("3");
-        assertTrue(set.remove("3"));
-        assertFalse(set.remove("5"));
+        assertTrue(set.remove("3")); //testing removing the head
+        assertEquals(0, set.returnNumberOfItem());
+        assertFalse(set.remove("5")); //testing removing an item not in the list
+        set.addItem("4");
         set.addItem("3");
         set.addItem("2");
-        assertTrue(set.remove("2"));
-        set.addItem("2");
-        assertTrue(set.remove("3"));
+        set.addItem("1");
+        assertTrue(set.remove("4")); //testing removing the last item
+        assertTrue(set.remove("2")); //testing removing an item in the middle
     }
 
     @Test
@@ -50,6 +57,15 @@ public class SetTest {
 
     @Test
     public void testUnion() {
-
+        Set<Integer> set1 = new Set<>();
+        Set<Integer> set2 = new Set<>();
+        set1.addItem(3);
+        set1.addItem(2);
+        set1.addItem(1);
+        set2.addItem(5);
+        assertEquals(0,set1.union(set2).returnNumberOfItem());
+        set2.addItem(4);
+        set2.addItem(3);
+        assertEquals(5,set1.union(set2).returnNumberOfItem());
     }
 }
