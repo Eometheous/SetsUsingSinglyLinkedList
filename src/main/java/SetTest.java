b3c3a928de02557974dd914677c763bcfe4958e7
Eometheous <jonathan.sthomas@icloud.com>
2022-09-14 19:07:46 -0700

    Made each test show how long it took.

--- src/main/java/SetTest.java
+++ src/main/java/SetTest.java
@@ -5,6 +5,8 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class SetTest {
     static Set<String> set;
+    double start, end;
+
     @BeforeAll
     public static void setUp() {
         set = new Set<>();
@@ -12,13 +14,26 @@ public class SetTest {
 
     @Test
     public void testAddItem() {
+        start = System.currentTimeMillis();
         assertTrue(set.addItem("3"));
         assertFalse(set.addItem("3"));
         assertEquals(1, set.numberOfItems());
+        end = System.currentTimeMillis();
+        System.out.println("Execution time of testAddItem: " + (end - start));
+    }
+    @Test
+    public void testInSet() {
+        start = System.currentTimeMillis();
+        set.addItem("3");
+        assertTrue(set.inSet("3"));
+        assertFalse(set.inSet("5"));
+        end = System.currentTimeMillis();
+        System.out.println("Execution time of testInSet: " + (end - start));
     }
 
     @Test
     public void testSearch() {
+        start = System.currentTimeMillis();
         set.addItem("3");
 
         Node<String> node = set.search("3");
@@ -26,10 +41,13 @@ public class SetTest {
         assertEquals(node, set.search("3"));
         assertEquals("3", set.search("3").getItem());
         assertNull(set.search("5"));
+        end = System.currentTimeMillis();
+        System.out.println("Execution time of testSearch: " + (end - start));
     }
 
     @Test
     public void testRemove() {
+        start = System.currentTimeMillis();
         set.addItem("3");
         assertTrue(set.remove("3")); //testing removing the head
         assertEquals(0, set.numberOfItems());
@@ -40,10 +58,13 @@ public class SetTest {
         set.addItem("1");
         assertTrue(set.remove("4")); //testing removing the last item
         assertTrue(set.remove("2")); //testing removing an item in the middle
+        end = System.currentTimeMillis();
+        System.out.println("Execution time of testRemove: " + (end - start));
     }
 
     @Test
     public void testIntersection() {
+        start = System.currentTimeMillis();
         Set<Integer> set1 = new Set<>();
         Set<Integer> set2 = new Set<>();
         set1.addItem(3);
@@ -53,10 +74,13 @@ public class SetTest {
         set2.addItem(3);
         assertEquals(1, set1.intersection(set2).numberOfItems());
         assertEquals(3, set1.intersection(set2).search(3).getItem());
+        end = System.currentTimeMillis();
+        System.out.println("Execution time of testIntersection: " + (end - start));
     }
 
     @Test
     public void testUnion() {
+        start = System.currentTimeMillis();
         Set<Integer> set1 = new Set<>();
         Set<Integer> set2 = new Set<>();
         set1.addItem(3);
@@ -67,5 +91,7 @@ public class SetTest {
         set2.addItem(4);
         set2.addItem(3);
         assertEquals(5,set1.union(set2).numberOfItems());
+        end = System.currentTimeMillis();
+        System.out.println("Execution time of testUnion: " + (end - start));
     }
 }
